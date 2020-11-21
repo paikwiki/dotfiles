fbaa8526d83648364d445fb7fbad8edc50d6ad1a
paikwiki <paikwiki@gmail.com>
2020-11-21 17:28:49 +0900

    Edit bootstrap.sh and delete Mackup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,96 +1,77 @@
 #!/bin/sh
 
 #-------------------------------------------------------------------------------
-# Thanks Appkr! https://github.com/appkr/dotfiles/blob/master/bootstrap.sh
+# Dotfiles
 #-------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
 # Set temporary variable
-#-------------------------------------------------------------------------------
-
 DOTFILES=$HOME/dotfiles
 
-#-------------------------------------------------------------------------------
 # Update dotfiles itself first
-#-------------------------------------------------------------------------------
-
 [ -d "$DOTFILES/.git" ] && git --work-tree="$DOTFILES" --git-dir="$DOTFILES/.git" pull origin master
 
 #-------------------------------------------------------------------------------
-# Check for Homebrew and install if it doesn't exist
+# Homebrew
 #-------------------------------------------------------------------------------
 
+# Check for Homebrew and install if it doesn't exist
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-#-------------------------------------------------------------------------------
 # Update Homebrew recipes
-#-------------------------------------------------------------------------------
-
 brew update
 
-#-------------------------------------------------------------------------------
 # Install all our dependencies with bundle (See Brewfile)
-#-------------------------------------------------------------------------------
-
 brew tap homebrew/bundle
 brew bundle --file=$DOTFILES/Brewfile # Install binary & applications
 brew cleanup
 brew cask cleanup
 
 #-------------------------------------------------------------------------------
-# Install global Git configuration
+# Git
 #-------------------------------------------------------------------------------
 
+# Install global Git configuration
 ln -nfs $DOTFILES/.gitconfig $HOME/.gitconfig
 git config --global core.excludesfile $DOTFILES/.gitignore_global
 git config --global user.name "paikwiki"
 git config --global user.email "paikwiki@gmail.com"
 
 #-------------------------------------------------------------------------------
-# Install Mackup config
+# Vim
 #-------------------------------------------------------------------------------
 
-[ ! -f $HOME/.mackup.cfg ] && ln -nfs $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
-
-#-------------------------------------------------------------------------------
 # Install .vimrc
-#-------------------------------------------------------------------------------
-
 ln -nfs $DOTFILES/.vimrc $HOME/.vimrc
 
 #-------------------------------------------------------------------------------
-# Install .bash_profile
+# Bash
 #-------------------------------------------------------------------------------
 
+# Install .bash_profile
 ln -nfs $DOTFILES/.bash_profile $HOME/.bash_profile
 
-#-------------------------------------------------------------------------------
 # Install .bash_aliases
-#-------------------------------------------------------------------------------
-
 ln -nfs $DOTFILES/.bash_aliases $HOME/.bash_aliases
 
 #-------------------------------------------------------------------------------
-# Install Composer
+# PHP
 #-------------------------------------------------------------------------------
 
+# Install Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-#-------------------------------------------------------------------------------
 # Install global Composer packages
-#-------------------------------------------------------------------------------
-
 /usr/local/bin/composer global require laravel/installer laravel/envoy
 
 #-------------------------------------------------------------------------------
-# Install global Node packages
+# Node
 #-------------------------------------------------------------------------------
 
+# Install global Node packages
 npm install gulp-cli gulp yo http-server nodemon --global --save
-npm install gitbook-cli --global --save
 
 #-------------------------------------------------------------------------------
 # Install Rails & Jekyll
